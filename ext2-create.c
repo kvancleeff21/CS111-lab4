f6df2d3cd88e0e24fe35e84efee9b263c0f35891
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-11-28 11:45:05 -0800

    added root

--- ext2-create.c
+++ ext2-create.c
@@ -365,6 +365,20 @@ void write_inode_table(int fd) {
 
 	// TODO It's all yours
 	// TODO finish the inode entries for the other files
+	struct ext2_inode root_inode = {0};
+	root_inode.i_mode = EXT2_S_IFDIR | EXT2_S_IRUSR | EXT2_S_IWUSR  | EXT2_S_IXUSR  | EXT2_S_IRGRP  | EXT2_S_IXGRP  | EXT2_S_IROTH  | EXT2_S_IXOTH;
+	root_inode.i_uid = 0;
+	root_inode.i_gid = 0;
+	root_inode.i_atime = current_time;
+	root_inode.i_ctime = current_time;
+	root_inode.i_mtime = current_time;
+	root_inode.i_dtime = 0;
+	root_inode.i_links_count = 3;
+	root_inode.i_blocks = 2;
+	root_inode.i_block[0] = ROOT_DIR_BLOCKNO;
+	write_inode(fd, EXT2_ROOT_INO, &root_inode);
+
+
 	struct ext2_inode hello_world_inode = {0};
 	hello_world_inode.i_mode = EXT2_S_IFREG | EXT2_S_IRUSR | EXT2_S_IWUSR | EXT2_S_IRGRP | EXT2_S_IROTH;
 	hello_world_inode.i_uid = 1000;
@@ -378,6 +392,8 @@ void write_inode_table(int fd) {
 	hello_world_inode.i_blocks = 1;
 	hello_world_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_WORLD_INO, &hello_world_inode);
+
+	
 	struct ext2_inode hello_inode = {0};
 	hello_inode.i_mode = EXT2_S_IFLNK | EXT2_S_IRUSR | EXT2_S_IWUSR | EXT2_S_IRGRP | EXT2_S_IROTH;
 	hello_inode.i_uid = 1000;
