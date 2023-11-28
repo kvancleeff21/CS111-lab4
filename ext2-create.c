521cb542f3131881b1479b76fb65fcdd56293947
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-11-28 11:47:26 -0800

    fixing more fsck errors

--- ext2-create.c
+++ ext2-create.c
@@ -390,7 +390,7 @@ void write_inode_table(int fd) {
 	hello_world_inode.i_mtime = current_time;
 	hello_world_inode.i_dtime = 0;
 	hello_world_inode.i_links_count = 1;
-	hello_world_inode.i_blocks = 1;
+	hello_world_inode.i_blocks = 2;
 	hello_world_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_WORLD_INO, &hello_world_inode);
 
