df917f3d2bb0a52813a427b50cf2401fc4391712
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-11-28 11:49:46 -0800

    hiya

--- ext2-create.c
+++ ext2-create.c
@@ -406,7 +406,6 @@ void write_inode_table(int fd) {
 	hello_inode.i_dtime = 0;
 	hello_inode.i_links_count = 1;
 	hello_inode.i_blocks = 2;
-	hello_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_INO, &hello_inode);
 }
 
